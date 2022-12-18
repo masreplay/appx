f5b6bd8f7163b6dafdebc60779500c11156b49d4
M Atheer <55885230+masreplay@users.noreply.github.com>
2022-12-19 00:18:59 +0300

    add:

--- lib/client/type/type.dart
+++ lib/client/type/type.dart
@@ -1,4 +1,5 @@
 export 'id.dart';
 export 'email.dart';
+export 'html.dart';
 export 'package:intl_phone_field/phone_number.dart';
 export 'package:image_picker/image_picker.dart';
--- lib/shared_preference/boolean_preference.dart
+++ lib/shared_preference/boolean_preference.dart
@@ -1,7 +1,7 @@
 import 'package:appx/shared_preference/share_preference.dart';
 
-class BoolPreference extends ObjectSharedPreference<bool> {
-  const BoolPreference(super.sharedPreference, {required super.key});
+class BooleanPreference extends ObjectSharedPreference<bool> {
+  const BooleanPreference(super.sharedPreference, {required super.key});
 
   @override
   bool? maybeData() {
