3e303bf8b2f540a0a7a32dc120175f14ecd25cc7
M Atheer <55885230+masreplay@users.noreply.github.com>
2022-12-19 00:40:01 +0300

    fix:

--- lib/client/client.dart
+++ lib/client/client.dart
@@ -1,8 +1,8 @@
 export 'package:freezed_annotation/freezed_annotation.dart';
-
 export 'callbacks/callbacks.dart';
 export 'convertor/convertor.dart';
 export 'paginated.dart';
 export 'error_handler/error_handler.dart';
+export 'exceptions/exception_filter.dart';
 export 'annotations/annotations.dart';
 export 'type/type.dart';
