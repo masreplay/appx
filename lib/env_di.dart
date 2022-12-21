9379e7acce804c534210624e869c0c5790ecd1fc
M Atheer <55885230+masreplay@users.noreply.github.com>
2022-12-21 15:41:47 +0300

    add injectable

---
+++ lib/env_di.dart
@@ -0,0 +1,7 @@
+import 'package:injectable/injectable.dart';
+
+const String productionName = "production";
+const String mockName = "mock";
+
+const Named productionService = Named(productionName);
+const Named mockService = Named(mockName);
