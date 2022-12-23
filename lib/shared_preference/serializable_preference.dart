e8008e8b6428d12b53c7eca3b530b1d10bef31df
M Atheer <55885230+masreplay@users.noreply.github.com>
2022-12-23 11:10:26 +0300

    old:

--- lib/shared_preference/serializable_preference.dart
+++ lib/shared_preference/serializable_preference.dart
@@ -13,10 +13,10 @@ import 'package:shared_preferences/shared_preferences.dart';
 ///      : super(key: "AuthenticationKey");
 ///
 ///  @override
-///  Authentication _fromJson(JsonMap json) => Authentication.fromJson(json);
+///  Authentication fromJson(JsonMap json) => Authentication.fromJson(json);
 ///
 ///  @override
-///  JsonMap _toJson(Authentication value) => value.toJson();
+///  JsonMap toJson(Authentication value) => value.toJson();
 /// }
 abstract class SerializablePreference<T> implements ObjectSharedPreference<T> {
   @override
@@ -32,30 +32,30 @@ abstract class SerializablePreference<T> implements ObjectSharedPreference<T> {
   });
 
   @protected
-  JsonMap _toJson(T value);
+  JsonMap toJson(T value);
 
   @protected
-  T _fromJson(JsonMap json);
+  T fromJson(JsonMap json);
 
   /// Read data from [sharedPreference] by it's [key] and then [json.decode]
-  /// and [_fromJson] it
+  /// and [fromJson] it
   @mustCallSuper
   @override
   T? maybeData() {
-    final value = sharedPreference.get(key);
+    final value = sharedPreference.getString(key);
     try {
-      return value == null ? null : _fromJson(json.decode(value as String));
+      return value == null ? null : fromJson(json.decode(value));
     } catch (e) {
       return null;
     }
   }
 
-  /// Starting by [_toJson] then [json.encode] object and
+  /// Starting by [toJson] then [json.encode] object and
   /// Set it in the [sharedPreference] by it's [key]
   @mustCallSuper
   @override
   Future<void> setData(T value) async {
-    final JsonMap newValue = _toJson(value);
+    final JsonMap newValue = toJson(value);
     final data = json.encode(newValue);
     await sharedPreference.setString(key, data);
   }
